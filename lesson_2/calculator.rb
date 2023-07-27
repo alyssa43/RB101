2f48b0d33d69249924099b84d059a751c0cb17bf
Alyssa Easter <AlyssaStarley@gmail.com>
2023-07-27 15:58:26 -0700

    creating repository and first lesson in lesson_2

---
+++ lesson_2/calculator.rb
@@ -0,0 +1,23 @@
+Kernel.puts('Welcome to Calculator!')
+
+Kernel.puts("What's the first number?")
+number1 = Kernel.gets.chomp
+
+Kernel.puts("What's the second number?")
+number2 = Kernel.gets.chomp
+
+Kernel.puts('What operation would you like to operform? 1) add 2) subtract 3) multiply 4) divide')
+operator = Kernel.gets.chomp
+
+result = case operator
+         when '1'
+           number1.to_i + number2.to_i
+         when '2'
+           number1.to_i - number2.to_i
+         when '3'
+           number1.to_i * number2.to_i
+         else
+           number1.to_f / number2
+         end
+
+Kernel.puts("The result is: #{result}")
