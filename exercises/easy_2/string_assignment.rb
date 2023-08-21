38078d8c29ac8cfa0af0fa76bde5f572c794d845
Alyssa Easter <AlyssaStarley@gmail.com>
2023-08-20 21:14:17 -0700

    Deleted renamed files

--- exercises/easy_2/string_assignment.rb
+++
@@ -1,8 +0,0 @@
-=begin
-What does this print out? Can you explain these results?
-  name = 'Bob'
-  save_name = name
-  name.upcase!
-  puts name, save_name
-
-This will print 'BOB' 'BOB' because when the "name" variable is called with ".upcase!" it mutates the caller (name), which then also changes the value of the "save_name" variable.
